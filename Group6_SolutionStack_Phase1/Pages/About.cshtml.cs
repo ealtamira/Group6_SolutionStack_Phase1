5ea53537d3e30a761415e5f787c9fd6916f5bc57
EAltamirano <ealtamira705@gmail.com>
2026-05-13 00:08:56 -0600

    feat:Add templete to all pages, Made About, Contact. Filled Contact page with req form. Added Info to About Us for my side, Made a basic Home page

---
+++ Group6_SolutionStack_Phase1/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Group6_SolutionStack_Phase1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
